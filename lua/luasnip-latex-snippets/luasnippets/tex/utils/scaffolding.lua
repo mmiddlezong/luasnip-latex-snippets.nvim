3bdb927b9d28aa97c8483c9140d34f605c787439
mmiddlezong <119022671+mmiddlezong@users.noreply.github.com>
2024-04-26 11:25:34 -0400

    Fix sqrt thing

--- lua/luasnip-latex-snippets/luasnippets/tex/utils/scaffolding.lua
+++ lua/luasnip-latex-snippets/luasnippets/tex/utils/scaffolding.lua
@@ -34,19 +34,29 @@ M = {}
 
 -- postfix helper function - generates dynamic node
 local generate_postfix_dynamicnode = function(_, parent, _, user_arg1, user_arg2)
-    local capture = parent.snippet.env.POSTFIX_MATCH
-    if #capture > 0 then
-        return sn(nil, fmta([[
+	local capture = parent.snippet.env.POSTFIX_MATCH
+	if #capture > 0 then
+		return sn(
+			nil,
+			fmta(
+				[[
         <><><><>
         ]],
-        {t(user_arg1), t(capture), t(user_arg2), i(0)}))
-    else
-        local visual_placeholder = parent.snippet.env.SELECT_RAW
-        return sn(nil, fmta([[
+				{ t(user_arg1), t(capture), t(user_arg2), i(0) }
+			)
+		)
+	else
+		local visual_placeholder = parent.snippet.env.SELECT_RAW
+		return sn(
+			nil,
+			fmta(
+				[[
         <><><><>
         ]],
-        {t(user_arg1), i(1, visual_placeholder), t(user_arg2), i(0)}))
-    end
+				{ t(user_arg1), i(1, visual_placeholder), t(user_arg2), i(0) }
+			)
+		)
+	end
 end
 
 -- visual util to add insert node - thanks ejmastnak!
@@ -67,17 +77,20 @@ M.auto_backslash_snippet = function(context, opts)
 	context.dscr = context.dscr or (context.trig .. "with automatic backslash")
 	context.name = context.name or context.trig
 	context.docstring = context.docstring or ([[\]] .. context.trig)
-    context.trigEngine = "ecma"
-    context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
-	return autosnippet(context,
-    fmta([[
+	context.trigEngine = "ecma"
+	context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
+	return autosnippet(
+		context,
+		fmta(
+			[[
     \<><>
     ]],
-    { f(function(_, snip)
-        return snip.captures[1]
-    end),
-    i(0) }),
-    opts)
+			{ f(function(_, snip)
+				return snip.captures[1]
+			end), i(0) }
+		),
+		opts
+	)
 end
 
 -- Auto symbol
@@ -90,10 +103,10 @@ M.symbol_snippet = function(context, command, opts)
 	context.name = context.name or command:gsub([[\]], "")
 	context.docstring = context.docstring or (command .. [[{0}]])
 	context.wordTrig = context.wordTrig or false
-    if opts.backslash == true then
-        context.trigEngine = "ecma"
-        context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
-    end
+	if opts.backslash == true then
+		context.trigEngine = "ecma"
+		context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
+	end
 	return autosnippet(context, t(command), opts)
 end
 
@@ -117,15 +130,22 @@ M.single_command_snippet = function(context, command, opts, ext)
 		docstring = [[{]] .. [[<1>]] .. [[}]] .. [[\label{(]] .. ext.short .. [[:<2>)?}]] .. [[<0>]]
 		ext.short = ext.short or command
 		lnode =
-			c(2 + (offset or 0), { t(""), sn(
-				nil,
-				fmta(
-					[[
+			c(2 + (offset or 0), {
+				t(""),
+				sn(
+					nil,
+					fmta(
+						[[
         \label{<>:<>}
         ]],
-					{ t(ext.short), i(1) }
-				)
-			) })
+						{ t(ext.short), i(1) }
+					)
+				),
+			})
+	end
+	if opts.backslash == true then
+		context.trigEngine = "ecma"
+		context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
 	end
 	context.docstring = context.docstring or (command .. docstring)
 	-- stype = ext.stype or s
@@ -136,8 +156,8 @@ M.single_command_snippet = function(context, command, opts, ext)
 	)
 end
 
-M.postfix_snippet = function (context, command, opts)
-    opts = opts or {}
+M.postfix_snippet = function(context, command, opts)
+	opts = opts or {}
 	if not context.trig then
 		error("context doesn't include a `trig` key which is mandatory", 2)
 	end
@@ -146,9 +166,13 @@ M.postfix_snippet = function (context, command, opts)
 	end
 	context.dscr = context.dscr
 	context.name = context.name or context.dscr
-    context.docstring = command.pre .. [[(POSTFIX_MATCH|VISUAL|<1>)]] .. command.post
-    context.match_pattern = [[[%w%.%_%-%"%']*$]]
-    return postfix(context, {d(1, generate_postfix_dynamicnode, {}, { user_args = {command.pre, command.post} })}, opts)
+	context.docstring = command.pre .. [[(POSTFIX_MATCH|VISUAL|<1>)]] .. command.post
+	context.match_pattern = [[[%w%.%_%-%"%']*$]]
+	return postfix(
+		context,
+		{ d(1, generate_postfix_dynamicnode, {}, { user_args = { command.pre, command.post } }) },
+		opts
+	)
 end
 
 return M
